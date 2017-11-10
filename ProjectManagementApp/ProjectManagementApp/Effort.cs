05f13bd6476c5d229fcc36d83ce31eb6581595c3
PastaWolf <blackhrulez95@yahoo.com>
2017-11-10 01:53:09 -0500

    Added Effort.cs

---
+++ ProjectManagementApp/ProjectManagementApp/Effort.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementApp
+{
+    class Effort
+    {
+        private int hoursExpended;
+        private DateTime timeStamps;
+        private string category;
+
+        public Effort(string category, int hoursExpended)
+        {
+            this.category = category;
+            this.hoursExpended = hoursExpended;
+            this.timeStamps = DateTime.Now;
+        }
+    }
+}
