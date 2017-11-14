38f9d07eb1e187d35ab58fe98932de9bd89fc466
JHBlancs <jhblancs@gmail.com>
2017-11-13 20:42:56 -0500

    Added UI for Project View
    Quite a bit of editing wizardry will be necessary for that come next time to touch it, but it's visually where i want it.

---
+++ ProjectManagementApp/ProjectManagementApp/ProjectViewForm.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectManagementApp
+{
+    public partial class ProjectViewForm : Form
+    {
+        public ProjectViewForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AddNonfuncReqButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddFunctReqButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddEffortButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DelSelFuncReqButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DelSelNonFuncReqButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EditProjectInfoButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BackToProjectSelectButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FunctionalReqsListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NonfuncReqsListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ProjectDescriptionTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TeamMembersListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RisksListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
